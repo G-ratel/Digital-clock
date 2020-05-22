35f116a9c480af26738e60482e924a9293fce048
Grace Ajiboye <60976458+G-ratel@users.noreply.github.com>
2020-05-22 14:58:32 +0100

    digital-clock

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    if (h == 0) {
+        h = 24;
+    }
+    if (h > 24) {
+        h = h - 24;
+    }
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s;
+    document.getElementById("myClock").innerText = time;
+    document.getElementById("myClock").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+showTime();
